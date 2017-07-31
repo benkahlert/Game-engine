fc659ec8f323657f7bf914e2c2eb6dd25118015b
Ben Kahlert <bkahlert@purdue.edu>
2017-07-31 14:49:39 -0400

    Added thread logistics to Game class

--- src/Game.java
+++ src/Game.java
@@ -1,13 +1,36 @@
-public class Game {
+public class Game implements Runnable {
 
+    // Screen dimensions
     public static int width = 300;
     public static int height = (width / 16) * 9;
     public static int scale = 3;
 
+    // Game thread variables
     private Thread gameThread;
+    private boolean running = false;
 
+    // Start game thread
     public synchronized void start() {
-        gameThread = new Thread();
+        this.running = true;
+        this.gameThread = new Thread(this, "Display");
+        this.gameThread.start();
+    }
+
+    // Stop game thread
+    public synchronized void stop() {
+        this.running = false;
+        try {
+            this.gameThread.join();
+        } catch (InterruptedException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    // Run method from Runnable interface for threads
+    public void run() {
+        while (running) {
+
+        }
     }
 
 }
