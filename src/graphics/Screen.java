44f4c5c9299e2b6fda0440b5761299025965c8a5
Ben Kahlert <bkahlert@purdue.edu>
2017-08-01 12:04:50 -0400

    Added randomly generated color tiles

--- src/graphics/Screen.java
+++ src/graphics/Screen.java
@@ -1,23 +1,34 @@
 package graphics;
 
+import java.util.Random;
+
 public class Screen {
 
-    // Pixels and dimensions
+    // Pixels, tiles, and dimensions
     private int width, height;
     public int[] pixels;
+    public int[] tiles = new int[64 * 64];
+
+    private Random random = new Random();
 
     // Instantiate screen with given height and width
     public Screen(int width, int height) {
         this.width = width;
         this.height = height;
         pixels =  new int[width * height];
+        for (int i = 0; i < tiles.length; i++) {
+            tiles[i] = random.nextInt(0xffffff);
+        }
     }
 
     // Renders the screen's pixels
     public void render() {
         for (int i = 0; i < height; i++) {
+            if (i < 0 || i >= height) { break; };
             for (int j = 0; j < width; j++) {
-                pixels[j + i * width] = 0xff00ff;
+                if (j < 0 || j >= width) { break; };
+                int tileIndex = (j >> 4) + (i >> 4) * 64;
+                pixels[j + i * width] = tiles[tileIndex];
             }
         }
     }
