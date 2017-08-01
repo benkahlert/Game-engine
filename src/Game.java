2ab72b35ff573bb7ce048e803bbaee79cda44178
Ben Kahlert <bkahlert@purdue.edu>
2017-08-01 11:53:27 -0400

    Added FPS counter in run method of Game class

--- src/Game.java
+++ src/Game.java
@@ -52,10 +52,31 @@ public class Game extends Canvas implements Runnable {
 
     // Run method from Runnable interface for threads
     public void run() {
+        long lastTime = System.nanoTime();
+        long timer = System.currentTimeMillis();
+        final double ns = 1000000000.0 / 60.0;
+        double delta = 0;
+        int frames = 0;
+        int updates = 0;
         while (running) {
-            update();
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1) {
+                update();
+                updates++;
+                delta--;
+            }
             render();
+            frames++;
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println("Updates: " + updates + ", Frames: " + frames);
+                updates = 0;
+                frames = 0;
+            }
         }
+        stop();
     }
 
     // Logic tick
--- src/graphics/Screen.java
+++ src/graphics/Screen.java
@@ -1,18 +1,19 @@
 package graphics;
 
-import java.awt.*;
-
 public class Screen {
 
+    // Pixels and dimensions
     private int width, height;
     public int[] pixels;
 
+    // Instantiate screen with given height and width
     public Screen(int width, int height) {
         this.width = width;
         this.height = height;
         pixels =  new int[width * height];
     }
 
+    // Renders the screen's pixels
     public void render() {
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
@@ -21,6 +22,7 @@ public class Screen {
         }
     }
 
+    // Clears each pixel
     public void clear() {
         for (int i = 0; i < pixels.length; i++) {
             pixels[i] = 0;
