b8f9f250329a265883786805dad973327464891f
Ben Kahlert <bkahlert@purdue.edu>
2017-08-07 13:13:49 -0400

    Tiles are now rendered in the Level class render method

--- src/level/Level.java
+++ src/level/Level.java
@@ -45,7 +45,13 @@ public class Level {
         int xEnd = (xScroll + screen.width) >> 4;
         int yStart = yScroll >> 4;
         int yEnd = (yScroll + screen.height) >> 4;
-        Tile tile = getTile(xStart, yStart);
+
+        for (int i = yStart; i < yEnd; i++) {
+            for (int  j = xStart; j < xEnd; j++) {
+                Tile tile = getTile(j, i);
+                tile.render(j, i, screen);
+            }
+        }
 
     }
 
