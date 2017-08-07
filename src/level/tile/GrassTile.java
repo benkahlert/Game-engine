c5155270a83f7f4a6da49e3904d15c78ca75395a
Ben Kahlert <bkahlert@purdue.edu>
2017-08-06 21:20:45 -0400

    Implemented render method in GrassTile class

--- src/level/tile/GrassTile.java
+++ src/level/tile/GrassTile.java
@@ -5,12 +5,14 @@ import graphics.Sprite;
 
 public class GrassTile extends Tile {
 
+    // Instantiate GrassTile
     public GrassTile(Sprite sprite) {
         super(sprite);
     }
 
+    // Actually render the tile
     public void render(int x, int y, Screen screen) {
-
+        screen.renderTile(x, y, this);
     }
 
 }
